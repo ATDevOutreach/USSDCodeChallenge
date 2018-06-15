b1a7f7f951557a22a9476b00fd56c5a258c4b275
Peter Olayinka <peterolayinka97@gmail.com>
2018-06-15 09:20:22 +0100

    heroku: build now

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn ussd_project.wsgi
+web: gunicorn config.wsgi --log-file -
--- config/urls.py
+++ config/urls.py
@@ -1,4 +1,4 @@
-"""ussd_project URL Configuration
+"""py_2_africaTalkingUSSD URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from ussd_app import views
 
 urlpatterns = [
+    url(r'', views.process_ussd, name="process_ussd"),
     url(r'^admin/', admin.site.urls),
 ]
