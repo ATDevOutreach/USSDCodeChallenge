0f5dfe6d9a9e6c16ce7ce40e8b5a9f7d2f9925d6
Justine Philip <philip.obosi@eng.uniben.edu>
2018-06-17 08:43:36 +0100

    pushed to heroku

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'SMS API APP' });
+  res.render('index', { title: 'USSD API APP' });
 });
 
 module.exports = router;
