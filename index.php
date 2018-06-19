95124deaf63dd083fadd9dc879b59d9370c01e0d
brianokinyi <brianokinyi.bo@gmail.com>
2018-06-19 01:11:18 +0000

    Main menu

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+    // Ensure ths code runs only after a POST from AT
+    if(!empty($_POST) && !empty($_POST['phoneNumber'])){
+        require_once('dbConnector.php');
+        require_once('AfricasTalkingGateway.php');
+        require_once('config.php');
+
+        //2. receive the POST from AT
+        $sessionId     =$_POST['sessionId'];
+        $serviceCode   =$_POST['serviceCode'];
+        $phoneNumber   =$_POST['phoneNumber'];
+        $ussdString          =$_POST['text'];
+
+        $level =0; 
+
+        if($ussdString != ""){  
+            $ussdString=  str_replace("#", "*", $ussdString);  
+            $ussdString_explode = explode("*", $ussdString);
+            $level = count($ussdString_explode);  
+        }
+
+        //echo ussd_text
+        function ussd_proceed ($ussd_text){  
+            echo $ussd_text;  
+            //exit(0);  
+        }
+
+        if ($level==0){				
+            displaymenu();
+        }  
+    }
+
+    // Main menu
+    function displaymenu() {             
+        $ussd_text="CON \n1: My Cooperative\n2: Wazobia Loans\n3: Join Agbetuntu\n4: Request a call";  
+        ussd_proceed($ussd_text);  
+    }
+
+?>
