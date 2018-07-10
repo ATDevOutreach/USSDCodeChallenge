0508d2b5890e88c5c5dab11c63e0dce218e430db
teenoh <valentino@grit.systems>
2018-07-10 18:56:46 +0100

    set Procfile

--- ussd_app/ussd_app/wsgi.py
+++ ussd_app/ussd_app/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for config project.
+WSGI config for ussd_app project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,9 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ussd_app.settings")
 
 application = get_wsgi_application()
-
-from whitenoise.django import DjangoWhiteNoise
-application = DjangoWhiteNoise(application)
